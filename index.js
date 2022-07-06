3a35178b436467871cb3d93c1ad08aaed7b295a6
Giorgi <giorgi.kublashvili2000@gmail.com>
2022-07-06 20:23:27 +0400

    refactor code

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
 let num1 = 8
-
 let num2 = 2
-
 let sumEl = document.getElementById("sum-el")
-
 document.getElementById("num1-el").textContent = num1;
 document.getElementById("num2-el").textContent = num2;
-
-
-
 function add() {
-
     let result = num1 + num2
     sumEl.textContent = "Sum:" + result
-    
 }
-
 function sub() {
     let result = num1 - num2
     sumEl.textContent = "Sum:" + result 
 }
-
 function div() {
     let result = num1 / num2
     sumEl.textContent = "Sum:" + result
 }
-
 function mult() {
     let result = num1 * num2
     sumEl.textContent = "sum:" + result
